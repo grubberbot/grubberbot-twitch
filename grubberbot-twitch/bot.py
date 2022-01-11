c92646fa9e7368afb32807ed742132fd9232047b
Paul Terwilliger <paul.terwil@gmail.com>
2022-01-11 10:49:30 -0500

    restructuring repository

--- grubberbot-twitch/bot.py
+++ grubberbot-twitch/bot.py
@@ -1,83 +1,71 @@
 import os
 
 import pyttsx3
-from dotenv import load_dotenv
+import yaml
 from twitchobserver import Observer
 
-# for the tts
-load_dotenv()
-TWITCH_TOKEN = os.getenv("TWITCH_TOKEN")
+# TODO get list of mods instead of hard-coding mod nicknames
 
-# TODO get list of mods instead of hard-coding mod usernames
+# Path variables
+CREDENTIALS_PATH = "credentials/credentials.yml"
+TTS_USERS_PATH = "data/tts_users.yml"
+SCREEN_TEXT_PATH = "data/tts.txt"
+
 CHANNEL = "pawngrubber"
-MODS = [
-    "rkmartin1",
-    #'bec4use',
-    "eccentrichorse11",
-    "gurkiratsingh17",
-    "acuddlypuppie",
-    #'harappa234',
-    "sfdc_mike",
-    "monichess12",
-    "hamsterham88",
-    "punkcoreyoda",
-    "vitothemonkey",
-    "venuschess",
-    "soham_slp",
-    "krapig",
-    "pawngrubber",
-    "thefuxia",
-    "smarterchess",
-    "streamlabs",
-    "grubberbot",
-    "lalizig",
-    "paradajzcity",
-    "duellinksguy",
-    "thewretch2",
-    "vietd",
-    "poitaoforpresident",
-    "chesscomchris",
-    "mynameislegyon",
-    "f0rgetaboutit",
-    "zuname",
-    "rakshakthetall",
-    "drittman13",
-    "expired_febreeze",
-    "strance_02",
-    "marko_sreck",
-    "flyingseverus",
-]
+IGNORED_USER_TYPE = "ignore"
+REGULAR_USER_TYPE = "user"
 
-SKIP = [
-    "harappa234",
-]
+
+def load_yaml(filename):
+    with open(filename, "r") as f:
+        data = yaml.safe_load(f)
+    return data
 
 
 class TTS:
     def __init__(self):
         self.engine = self.build_engine()
 
+        self.user_types = {
+            "super_user": {
+                "voice": -2,
+                "volume": 1.0,
+            },
+            "user": {
+                "voice": -1,
+                "volume": 0.25,
+            },
+        }
+
+    def get_user_type(self, nickname):
+        data = load_yaml(TTS_USERS_PATH)
+        user_type = data[nickname] if nickname in data else None
+        return user_type
+
     def build_engine(self):
         engine = pyttsx3.init()
         engine.setProperty("rate", 150)
-        engine.setProperty("voice", engine.getProperty("voices")[-2].id)
         return engine
 
-    def chat_speak(self, message, nickname):
-        self.engine.setProperty("voice", self.engine.getProperty("voices")[-1].id)
-        self.engine.setProperty("volume", 0.25)
-        self.speak(message, nickname)
-
-    def mod_speak(self, message, nickname):
-        self.engine.setProperty("voice", self.engine.getProperty("voices")[-2].id)
-        self.engine.setProperty("volume", 1.0)
-        self.speak(message, nickname)
-
     def speak(self, message, nickname):
-        if "!tts" in message:
-            text = message.lstrip("!tts")
-            filename = "tts.txt"
-            with open(filename, "w") as f:
+        user_type = self.get_user_type(nickname)
+
+        if user_type == IGNORED_USER_TYPE:
+            return
+        elif user_type not in self.user_types:
+            user_type = REGULAR_USER_TYPE
+
+        # Set voice and volume
+        voice = self.user_types[user_type]["voice"]
+        volume = self.user_types[user_type]["volume"]
+        self.engine.setProperty("voice", self.engine.getProperty("voices")[voice].id)
+        self.engine.setProperty("volume", volume)
+
+        # Actually speak now
+        prefix = "!tts "
+        if message.lower().startswith(prefix):
+            text = message.lstrip(prefix)
+            with open(SCREEN_TEXT_PATH, "w") as f:
                 print(f"{nickname}: {text}", file=f)
             self.engine.say(text)
             self.engine.runAndWait()
@@ -85,30 +73,24 @@ class TTS:
 
 def main():
     tts = TTS()
+    e = None
 
-    with Observer("username", TWITCH_TOKEN) as observer:
+    credentials = load_yaml(CREDENTIALS_PATH)
+    with Observer("username", credentials["OAUTH"]) as observer:
         observer.join_channel(CHANNEL)
         print("Joined channel", CHANNEL)
-        print(
-            "Press Ctrl+C to quit, you might have to close the terminal window sometimes."
-        )
 
+        # Twitch loop
         while True:
             try:
                 for event in observer.get_events():
                     if event.type == "TWITCHCHATMESSAGE":
-                        message = event.message
-
-                        if event.nickname in MODS:
-                            tts.mod_speak(message, event.nickname)
-                        elif event.nickname not in SKIP:
-                            tts.chat_speak(message, event.nickname)
-
+                        tts.speak(event.message, event.nickname)
             except Exception as e:
                 print(e)
                 observer.leave_channel(CHANNEL)
-                observer.stop()
-                quit()
+                break
+    raise e
 
 
 if __name__ == "__main__":
